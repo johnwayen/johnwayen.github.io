f938acfb286b85fe2a9c10e717f5f7c63785e0c5
wei.jiang <75139166@qq.com>
2014-02-11 08:05:36 +0000

    静态博客提交

---
+++ johnwayen.github.io/javascripts/config.js
@@ -0,0 +1,6 @@
+var disqus_shortname = 'hooloodemo';
+var hostbase = 'http://johnwayen.github.io';
+var githubname = 'johnwayen';
+var repos = 'johnwayen.github.io';
+var sitetitle = '3jie';
+var rss = '';
