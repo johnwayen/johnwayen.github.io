b37dc6279f80149686f6ac8bf0aa5c4b1b5f9d99
johnwayen <75139166@qq.com>
2014-02-11 20:08:51 +0800

    Update config.js

--- javascripts/config.js
+++ javascripts/config.js
@@ -2,5 +2,5 @@ var disqus_shortname = 'hooloodemo';
 var hostbase = 'http://johnwayen.github.io';
 var githubname = 'johnwayen';
 var repos = 'johnwayen.github.io';
-var sitetitle = '3jie';
+var sitetitle = '一个程序猿，这么有福气~';
 var rss = '';
