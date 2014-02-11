12e4dc037fcf0cc83933a662a70ae1f169935b34
wei.jiang <75139166@qq.com>
2014-02-11 09:03:09 +0000

    提交

--- Hooloo-master/javascripts/config.js
+++
@@ -1,6 +0,0 @@
-var disqus_shortname = 'hooloodemo';
-var hostbase = 'http://johnwayen.github.io';
-var githubname = 'johnwayen';
-var repos = 'johnwayen.github.io';
-var sitetitle = '3jie';
-var rss = '';
